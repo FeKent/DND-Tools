78a2373357bb235091a527745a621a937c659421
Fiona Kent <kentf022@gmail.com>
2023-12-09 19:02:32 +0000

    Hook up Compose Navigation with screens

--- app/src/main/java/com/example/dndtools/MainActivity.kt
+++ app/src/main/java/com/example/dndtools/MainActivity.kt
@@ -6,16 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
+import com.example.dndtools.composables.AddCampaignScreen
+import com.example.dndtools.composables.InitialSetUpScreen
 import com.example.dndtools.data.DndToolsDatabase
 import com.example.dndtools.ui.theme.DNDToolsTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +47,16 @@ sealed class  Screen(val route: String){
 fun DndToolsApp() {
     val appContext = LocalContext.current
     val database = remember { Room.databaseBuilder(appContext, DndToolsDatabase::class.java, "Campaign Database").build()}
+    val navController = rememberNavController()
 
-    NavHost()
+    NavHost(navController = navController, startDestination = Screen.Intro.route){
+        composable(Screen.Intro.route){ InitialSetUpScreen()}
+        composable(Screen.Add.route){
+            val addScreenScope = rememberCoroutineScope()
+            AddCampaignScreen(onCampaignEntered = { newCampaign ->
+            addScreenScope.launch {
+                database.campaignDao().insertCampaign(newCampaign)
+            }
+        })}
+    }
 }
