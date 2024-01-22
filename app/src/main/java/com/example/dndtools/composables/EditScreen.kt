f20197818bb7057e87d75b93500a161bbf64a591
Fiona Kent <kentf022@gmail.com>
2024-01-22 12:11:14 +0000

    Add an Exposed Dropdown Menu Box to the Edit Screen for Adventure Type

--- app/src/main/java/com/example/dndtools/composables/EditScreen.kt
+++ app/src/main/java/com/example/dndtools/composables/EditScreen.kt
@@ -14,9 +14,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.ExposedDropdownMenuDefaults.TrailingIcon
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,6 +30,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,6 +46,8 @@ fun EditScreen(adventure: Adventure, back: () -> Unit) {
     var players by remember { mutableStateOf(adventure.players.toString()) }
     var setting by remember { mutableStateOf(adventure.setting) }
 
+    var expanded by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -48,11 +55,11 @@ fun EditScreen(adventure: Adventure, back: () -> Unit) {
     ) {
         CenterAlignedTopAppBar(
             title = {
-                    Text(
-                        text = "Edit ${adventure.adventureType.name}: ${adventure.title}",
-                        color = MaterialTheme.colorScheme.secondary,
-                        fontWeight = FontWeight.ExtraBold
-                    )
+                Text(
+                    text = "Edit ${adventure.adventureType.name}: ${adventure.title}",
+                    color = MaterialTheme.colorScheme.secondary,
+                    fontWeight = FontWeight.ExtraBold
+                )
             },
             colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.primary),
             modifier = Modifier.shadow(4.dp),
@@ -88,15 +95,45 @@ fun EditScreen(adventure: Adventure, back: () -> Unit) {
                     label = "Setting",
                     value = setting,
                     onValueChange = { setting = it })
+                Spacer(modifier = Modifier.size(8.dp))
+                ExposedDropdownMenuBox(expanded, onExpandedChange = { expanded = false }) {
+                    TextField(
+                        value = adventure.adventureType.name,
+                        onValueChange = {},
+                        readOnly = true,
+                        label = {
+                            Text(
+                                text = "Adventure Type", color = MaterialTheme.colorScheme.primary
+                            )
+                        },
+                        trailingIcon = { TrailingIcon(expanded) },
+                        textStyle = TextStyle(color = MaterialTheme.colorScheme.primary),
+                        colors = ExposedDropdownMenuDefaults.textFieldColors(
+                            focusedContainerColor = MaterialTheme.colorScheme.onPrimary,
+                            unfocusedContainerColor = MaterialTheme.colorScheme.onPrimary,
+                            disabledContainerColor = MaterialTheme.colorScheme.onPrimary,
+                        )
+
+                    )
+                }
             }
         }
     }
 }
 
+
 @Preview(showSystemUi = true)
 @Composable
 fun EditPreview() {
     DNDToolsTheme {
-        EditScreen(adventure = Adventure(1, AdventureType.OneShot,"Misfits", 3, "Sword's Coast")){}
+        EditScreen(
+            adventure = Adventure(
+                1,
+                AdventureType.OneShot,
+                "Misfits",
+                3,
+                "Sword's Coast"
+            )
+        ) {}
     }
 }
