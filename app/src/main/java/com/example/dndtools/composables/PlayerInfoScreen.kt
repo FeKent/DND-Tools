2817f4a26df6a00dbda6cf09be75a272b315f4b0
Kent <kentf022@gmail.com>
2024-02-05 18:18:25 +0000

    Add preview to Player Info Screen

--- app/src/main/java/com/example/dndtools/composables/PlayerInfoScreen.kt
+++ app/src/main/java/com/example/dndtools/composables/PlayerInfoScreen.kt
@@ -5,6 +5,7 @@ package com.example.dndtools.composables
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -24,7 +25,6 @@ import com.example.dndtools.data.Adventure
 
 @Composable
 fun PlayerInfoScreen(adventure: Adventure?, back: () -> Unit) {
-
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -55,5 +55,7 @@ fun PlayerInfoScreen(adventure: Adventure?, back: () -> Unit) {
                 )
             }
         )
+        Spacer(modifier = Modifier.size(52.dp))
     }
 }
+
